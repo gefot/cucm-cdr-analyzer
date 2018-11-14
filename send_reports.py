1fa987b0bf46261e87adfb8aef20ba9142c9aa19
gefot <georgios.fotiadis@gmail.com>
2018-11-14 19:08:57 +0200

    New

--- send_reports.py
+++ send_reports.py
@@ -5,23 +5,21 @@ import _module_send_mail
 
 ####################################################################################################
 #  MAIN
-# access = json.load(open('data/access.json'))                                # Windows
-access = json.load(open('/home/pbx/cucm-cdr-analyzer/data/access.json'))    # Linux
 
-USERNAME = str(access["gmail"]["username"])
-PASSWORD = str(access["gmail"]["password"])
-
-current_date = datetime.datetime.now().strftime('%B %Y')
-
+# access = json.load(open('data/access.json'))                            # Windows
 # body_file1 = 'data/hourly_html.txt'                                     # Windows
 # attachments1 = ['data/hourly_report.csv']                               # Windows
 # body_file2 = 'data/daily_html.txt'                                      # Windows
 # attachments2 = ['data/daily_report.csv']                                # Windows
 
-body_file1 = '/home/pbx/cucm-cdr-analyzer/data/hourly_html.txt'         # Linux
-attachments1 = ['/home/pbx/cucm-cdr-analyzer/data/hourly_report.csv']   # Linux
-body_file2 = '/home/pbx/cucm-cdr-analyzer/data/daily_html.txt'          # Linux
-attachments2 = ['/home/pbx/cucm-cdr-analyzer/data/daily_report.csv']    # Linux
+access = json.load(open('/home/pbx/cucm-cdr-analyzer/data/access.json'))  # Linux
+body_file1 = '/home/pbx/cucm-cdr-analyzer/data/hourly_html.txt'           # Linux
+attachments1 = ['/home/pbx/cucm-cdr-analyzer/data/hourly_report.csv']     # Linux
+body_file2 = '/home/pbx/cucm-cdr-analyzer/data/daily_html.txt'            # Linux
+attachments2 = ['/home/pbx/cucm-cdr-analyzer/data/daily_report.csv']      # Linux
+
+
+current_date = datetime.datetime.now().strftime('%B %Y')
 
 subject1 = "Hourly Call Report - {}".format(current_date)
 fd1 = open(body_file1,'r')
@@ -33,7 +31,8 @@ fd2 = open(body_file2,'r')
 body2 = fd2.read()
 fd2.close()
 
-
+USERNAME = str(access["gmail"]["username"])
+PASSWORD = str(access["gmail"]["password"])
 # toaddr = ["abhijit.dhar@whitehatvirtual.com","val.king@whitehatvirtual.com","floyd.willis@vvrmc.org", \
 #           "john.lomas@vvrmc.org","dgalma01@vvrmc.org","maricela.sandoval@amistadmp.org","cgroom01@vvrmc.org", \
 #           "Albert.Lattimer@vvrmc.org","Ricardo.Gonzalez@vvrmc.org","letty.ortiz@vvrmc.org","georgios.fotiadis@gmail.com"]
