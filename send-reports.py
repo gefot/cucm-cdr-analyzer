c69f1126eddf0f79b506823d39b20bd1eda8e8c6
gefot <georgios.fotiadis@gmail.com>
2018-11-14 18:56:21 +0200

    New

--- send-reports.py
+++ send-reports.py
@@ -11,19 +11,25 @@ PASSWORD = str(access["gmail"]["password"])
 
 current_date = datetime.datetime.now().strftime('%B %Y')
 
+# body_file1 = 'data/hourly_html.txt'                                     # Windows
+# attachments1 = ['data/hourly_report.csv']                               # Windows
+# body_file2 = 'data/daily_html.txt'                                      # Windows
+# attachments2 = ['data/daily_report.csv']                                # Windows
+
+body_file1 = '/home/pbx/cucm-cdr-analyzer/data/hourly_html.txt'         # Linux
+attachments1 = ['/home/pbx/cucm-cdr-analyzer/data/hourly_report.csv']   # Linux
+body_file2 = '/home/pbx/cucm-cdr-analyzer/data/daily_html.txt'          # Linux
+attachments2 = ['/home/pbx/cucm-cdr-analyzer/data/daily_report.csv']    # Linux
+
 subject1 = "Hourly Call Report - {}".format(current_date)
-body_file1 = 'data/hourly_html.txt'
 fd1 = open(body_file1,'r')
 body1 = fd1.read()
 fd1.close()
-attachments1 = ['data/hourly_report.csv']
 
 subject2 = "DailyCall Report - {}".format(current_date)
-body_file2 = 'data/daily_html.txt'
 fd2 = open(body_file2,'r')
 body2 = fd2.read()
 fd2.close()
-attachments2 = ['data/daily_report.csv']
 
 
 # toaddr = ["abhijit.dhar@whitehatvirtual.com","val.king@whitehatvirtual.com","floyd.willis@vvrmc.org", \
