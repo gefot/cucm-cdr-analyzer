9e796a9c6973abbce26baf3b760ba73b6221ac3b
gefot <georgios.fotiadis@gmail.com>
2018-11-20 18:31:11 +0200

    New

--- parse_cdr_latest.py
+++ parse_cdr_latest.py
@@ -2,13 +2,11 @@ import os
 import datetime
 import glob
 
-
 date = datetime.datetime.now()
 today = date.strftime('%Y')+date.strftime('%m')+date.strftime('%d')
 print(today)
 today = "20181119"
 
-#filename =
 files = glob.glob("/home/cdr/cdr_data/cdr*{}*".format(today))
 newest = max(files, key = os.path.getctime)
 print(newest)
